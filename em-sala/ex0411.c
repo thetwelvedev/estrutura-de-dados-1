314053ee82811ed83da944cb7c7ca24a590be5d8
Leonardo Castro <twelve.programming@gmail.com>
2024-11-06 00:13:09 -0400

    Exercício em sala aula 04/11

--- em-sala/ex0411.c
+++ em-sala/ex0411.c
@@ -3,32 +3,31 @@
 #include <time.h>
 
 int main(){
-    int vetor[5321];  // Declara um vetor de 5321 posições
-    int tamanho = sizeof(vetor) / sizeof(vetor[0]);  // Calcula o tamanho do vetor
+    int vetor[5321];
+    int tamanho = sizeof(vetor) / sizeof(vetor[0]);
     int colisoes, valorRandom;
 
-    // Inicializa o vetor com valores -1, indicando que as posições estão vazias
+    //Inicializa o vetor com valores -1, indicando que as posições estão vazias
     for (int i = 0; i < tamanho; i++){
         vetor[i] = -1;
     }
     
-    colisoes = 0;  // Inicializa o contador de colisões
-    srand((unsigned) time(NULL));  // Semente para geração de números aleatórios
+    colisoes = 0; 
+    srand((unsigned) time(NULL)); //Semente para geração de números aleatórios
     
-    // Gera 2000 números aleatórios e tenta armazená-los no vetor
+
     for(int i = 0; i < 2000; i++){
-        valorRandom = rand() % 1000000 + 1;  // Gera um número aleatório entre 1 e 1.000.000
+        valorRandom = rand() % 1000000 + 1; //números aleatórios no intervalo de 1 a 10^6
         
         // Verifica se a posição no vetor já está ocupada (colisão)
         if(vetor[valorRandom % tamanho] != -1){
-            colisoes++;  // Incrementa o contador de colisões
+            colisoes++;
         }
         
-        // Armazena o número aleatório na posição calculada
+        // Armazena o número aleatório na posição calculada -> inserções
         vetor[valorRandom % tamanho] = valorRandom;
     }
-    
-    // Imprime o número total de colisões
     printf("Numero de colisoes: %d\n", colisoes);
+    
     return 0;
 }
