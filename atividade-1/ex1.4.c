c4335543c039e0092e63626fafa6630207be600e
Leonardo Castro <twelve.programming@gmail.com>
2024-11-09 22:25:38 -0400

    Quarto exercício da lista(funções simples)

--- atividade-1/ex1.4.c
+++ atividade-1/ex1.4.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+//Soma dos n números naturais ímpares.
+
+int soma_impares(int n);
+
 int main(){
+    int number, resultado;
+
+    printf("Digite um numero: ");
+    scanf("%d", &number);
     
+    resultado = soma_impares(number);
+
+    printf("Numero %d: soma: %d\n", number, resultado);
+
+    return 0;
+}
+
+int soma_impares(int n){
+    int soma = 0; //Inicializo a variável para não ficar com lixo de memória
+    for(int i = 0; i <= n; i++){
+        if(i % 2 != 0){ //Detecta se é ímpar, tem que ser o i pois é cada valor até chegar em 5
+            soma += i;
+        }
+    }
+    return soma;
 }
