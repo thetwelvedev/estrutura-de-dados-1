e4d763ae8a707e7b5f339f71db17cc3056758021
Leonardo Castro <twelve.programming@gmail.com>
2024-11-09 21:56:29 -0400

    Terceiro exercício da lista(funções simples)

--- atividade-1/ex1.3.c
+++ atividade-1/ex1.3.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+//Retorne o n-ésimo termo da série de Fibonacci 1 1 2 3 5 8 13 21 ...
+//Os dois primeiros termos são 1 e cada termo sequinte f(n + 2)= f(n-1) + f(n-2)
+
+int fibonacci(int n);
+
 int main(){
+    int number, resultado;
+
+    printf("Digite um numero: ");
+    scanf("%d", &number);
     
+    resultado = fibonacci(number);
+
+    printf("termo %d: %d\n", number, resultado);
+
+    return 0;
+}
+
+int fibonacci(int n){//Usando a recursividade, onde ela vai chamar a própria função para pegar os valores
+    if(n <= 2){//Caso base
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 }
