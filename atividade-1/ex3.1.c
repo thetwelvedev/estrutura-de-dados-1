d17fb98dc96eb0f9a117c7e8f120f08121ea89e7
Leonardo Castro <twelve.programming@gmail.com>
2024-11-10 23:35:08 -0400

    Oitavo exercício da lista(vetores)

---
+++ atividade-1/ex3.1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Função que recebe como parâmetros um vetor de números reais(vet) de tamanho n e retorne quantos números negativos serão armazenados nesse vetor.*/
+
+int negativos(int n, float* vet);
+
+int main(){
+    int resultado, tamanho_vet;
+    float* vet;
+
+    printf("Tamanho para o vetor: ");
+    scanf("%d", &tamanho_vet);
+
+    vet = (float*) malloc(tamanho_vet*sizeof(float)); //Alocação dinâmica
+
+    for (int i = 0; i < tamanho_vet; i++){
+        printf("Digite um numero: ");
+        scanf("%f", &vet[i]);
+    }
+    resultado = negativos(tamanho_vet,vet);
+    printf("Qtd numeros negativos: %d", resultado);
+
+}
+
+int negativos(int n, float* vet){
+    int contador = 0;
+    for(int i = 0; i < n; i++){ //Vai pecorrer o vetor
+        if(vet[i] < 0){ //Quando o número for negativo
+            contador++;
+        }
+    }
+    return contador;
+}
