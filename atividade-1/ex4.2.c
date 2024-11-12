e8af22be1f27d99373808d72972bf4bede09130f
Leonardo Castro <twelve.programming@gmail.com>
2024-11-12 01:31:50 -0400

    Décimo quarto exercício da lista(matrizes)

--- atividade-1/ex4.2.c
+++ atividade-1/ex4.2.c
@@ -23,6 +23,7 @@ int main(){
     }
 
     //Printando a matriz triangular inferior
+    printf("Matriz Triangular Inferior\n");
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) { 
             if(i < j){
