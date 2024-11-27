4de38bccb427d4e4eaec47b808fa0c1c400dd68c
Leonardo Castro <twelve.programming@gmail.com>
2024-11-26 22:32:04 -0400

    Questões 4 e 5 a fazer

---
+++ em-sala/ex1311(4).c
@@ -0,0 +1,15 @@
+/*Faça uma função que compare duas strings, conforme vimos
+nesta aula.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void compararStrings(char* str1, char* str2){
+    
+}
+
+int main(){
+
+
+    return 0;    
+}
---
+++ em-sala/ex1311(5).c
@@ -0,0 +1,14 @@
+/*Verifique se uma palavra é um palíndromo.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void palindromo(char* str1){
+    
+}
+
+int main(){
+
+
+    return 0;    
+}
