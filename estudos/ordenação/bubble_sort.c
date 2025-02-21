e4c3f8350ab1bd3ad2ff11b1adc044a8be3ba189
Leonardo Castro <twelve.programming@gmail.com>
2025-02-21 11:56:57 -0400

    Atualizando o código do bubble sort

--- estudos/ordenação/bubble_sort.c
+++ estudos/ordenação/bubble_sort.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
-void bubble_Sort(int vet[], int tam) {
-    int i, j, temp;
-    int trocado;
+void bubble_sort(int* vet, int tam) {
+    int temp, troca;
 
-    // Percorre o array
-    for (i = 0; i < tam - 1; i++) {
-        trocado = 0; // Flag para saber se houve troca 
-
-        for (j = 0; j < tam - i - 1; j++) { //Diminui a comparação a cada iteração
-            if (vet[j] > vet[j + 1]) { // Compara elementos adjacentes
-                // Troca os elementos
+    for(int i = 0; i < tam - 1; i++){
+        troca = 0; //Antes de cada iteração é resetado a troca
+        for(int j = 0; j < tam - 1 - i; j++){ //Como cada iteração o elemento maior vai para o final adicona o -i
+            if(vet[j] > vet[j+1]){
                 temp = vet[j];
-                vet[j] = vet[j + 1];
-                vet[j + 1] = temp;
-                trocado = 1; // Vai informar que houve troca
+                vet[j] = vet[j+1];
+                vet[j+1] = temp;
+                troca++;
             }
         }
-        if (trocado == 0) //0 trocas significa que está ordenado
+        if(troca == 0){
             break;
+        }
     }
 }
 
@@ -34,12 +31,12 @@ int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Array antes da ordenação: ");
+    printf("Array antes da ordenacao: ");
     printArray(arr, n);
 
-    bubbleSort(arr, n);
+    bubble_sort(arr, n);
 
-    printf("Array depois da ordenação: ");
+    printf("Array depois da ordenacao: ");
     printArray(arr, n);
 
     return 0;
